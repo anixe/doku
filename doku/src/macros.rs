0e2ecfcaf2ecf25688ff70402f0d1e8d1819915b
Patryk Wychowaniec <pwychowaniec@pm.me>
2022-10-21 11:53:34 +0200

    macros: Unexport `swrite`, hide `assert_doc`

--- doku/src/macros.rs
+++ doku/src/macros.rs
@@ -1,11 +1,24 @@
+#[doc(hidden)]
 #[macro_export]
 macro_rules! assert_doc {
-    ($expected:expr, $actual:expr) => {
-        assert_eq!(indoc::indoc! { $expected }.trim(), $actual.trim());
-    };
+    ($expected:expr, $actual:expr) => {{
+        use ::difference::Changeset;
+
+        let expected = indoc::indoc! { $expected }.trim();
+        let actual = $actual;
+        let actual = actual.trim();
+
+        if expected != actual {
+            panic!(
+                "\n<expected>\n{}\n</expected>\n\n<actual>\n{}\n</actual>\n\n<diff>\n{}\n</diff>",
+                expected,
+                actual,
+                Changeset::new(&actual, &expected, "\n").to_string().trim(),
+            );
+        }
+    }};
 }
 
-#[macro_export]
 macro_rules! swrite {
     ($target:expr, if $expr:expr, $($tt:tt)+) => {
         if $expr {
