e0142b221ab2086b332f8bc4b888bfdf88a15774
Karol Belina <kbl@anixe.pl>
2026-09-07 10:44:59 +0200

    json: Do not print a trailing comma for adjacently-tagged unit variants

--- doku/src/printers/json/print_enum/separated/multiline.rs
+++ doku/src/printers/json/print_enum/separated/multiline.rs
@@ -138,16 +138,16 @@ fn print_variant<'ty>(
             ctxt.out.inc_indent();
             ctxt.out.write_key_and_separator(tag);
             ctxt.out.write(format!(r#""{}""#, variant.id));
-            ctxt.out.write_property_separator_ln();
 
             if let Fields::Named { .. } | Fields::Unnamed { .. } =
                 variant.fields
             {
+                ctxt.out.write_property_separator_ln();
                 ctxt.out.write_key_and_separator(content);
                 ctxt.print_fields(&variant.fields, None);
-                ctxt.out.ln();
             }
 
+            ctxt.out.ln();
             ctxt.out.dec_indent();
             ctxt.out.write("}");
         }
